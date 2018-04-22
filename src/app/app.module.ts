2fc405be5c11c0dac6f0ec07b0eaefcab0324b70
lordcndy000 <lordwill.mabalot@gmail.com>
2018-04-22 20:43:55 +0800

    Added search form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatToolbarModule } from '@angular/material/toolbar'
-
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon'
 import { AppComponent } from './app.component'
 import { NavBarComponent } from './components/nav-bar/nav-bar.component'
+import { SearchformComponent } from './components/searchform/searchform.component'
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  declarations: [AppComponent, NavBarComponent, SearchformComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
