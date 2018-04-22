eb657e6d233b3990caa2a8a87f46afe80ba35afa
lordcndy000 <lordwill.mabalot@gmail.com>
2018-04-22 20:29:56 +0800

    Added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 import { AppComponent } from './app.component'
+import { NavBarComponent } from './components/nav-bar/nav-bar.component'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
