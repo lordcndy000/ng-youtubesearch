6d925b91b0ea3d05c88ce6cf26e1a4b154c15ace
lordcndy000 <lordwill.mabalot@gmail.com>
2018-05-04 19:38:39 +0800

    Added api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true
-};
+}
+
+export const youtubeKeys = {
+  apiKey: 'AIzaSyD8udxtCuHDKxYggd6i_QdE46mDNTC8HIk',
+  apiUrl: 'https://www.googleapis.com/youtube/v3/search'
+}
